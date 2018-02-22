0a1f1be9708ad8238ed3c40320bc1fa71e6c4cf9
Evan Hsu <evanhsu@gmail.com>
2018-02-22 11:45:27 -0700

    Added Handbrake to apt packages. Run.sh script now accepts the --tags argument

--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
 #! /bin/bash
-ansible-playbook -i inventory main.yml --ask-become-pass
+SHORTOPTS="t:"
+LONGOPTS="tags:"
+ARGS=$(getopt -s bash --options ${SHORTOPTS} --longoptions ${LONGOPTS} -- "$@" )
+eval set -- ${ARGS}
+
+while true; do
+    case ${1} in
+        -t | --tags)
+            shift
+            TAGS="$1"
+            shift
+            ;;
+        -- )
+            shift
+            break
+            ;;
+        * )
+            shift
+            break
+            ;;
+    esac
+done
+
+if [[ -n $TAGS ]]; then
+    ansible-playbook -i inventory main.yml --ask-become-pass --tags "$TAGS"
+else
+    ansible-playbook -i inventory main.yml --ask-become-pass
+fi
 
